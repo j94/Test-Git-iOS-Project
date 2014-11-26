2e7bdaa04277aed52d15fb5d86b44c125ccb7e90
James Carney <James@Jamess-MacBook-Air.local>
2014-11-26 01:01:57 -0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by James Carney on 11/26/14.
+//  Copyright (c) 2014 James Carney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by James Carney on 11/26/14.
+//  Copyright (c) 2014 James Carney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
