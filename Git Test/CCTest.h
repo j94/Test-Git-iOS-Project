f55ce12dda85963e3e1077c9dc126d0fa77e95b0
James Carney <James@Jamess-MacBook-Air.local>
2014-11-30 23:08:20 -0800

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by James Carney on 11/30/14.
+//  Copyright (c) 2014 James Carney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
